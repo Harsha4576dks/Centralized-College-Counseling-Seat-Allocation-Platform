e07b262ab868c50288ebe4e6cbab3bdc5f611b8b
Harsha4576dks <harshivahm@gmail.com>
2026-09-13 00:20:13 +0530

    login credentials

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+from ..models.user import User
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+
+def create_user(db: Session, user):
+    db_user = User( username=user.username,  email=user.email, password=user.password )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
